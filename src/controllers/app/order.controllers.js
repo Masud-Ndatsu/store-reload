969cb4af9735758ff392237c25350f98156d716e
Masud-Ndatsu <masudndatsu@gmail.com>
2023-10-13 02:15:08 +0100

    feat: modified user product and order controller

--- src/controllers/app/order.controllers.js
+++ src/controllers/app/order.controllers.js
@@ -1,128 +1,128 @@
 import OrderService from "../../services/app/order.service";
 
 const addToCart = async (req, res, next) => {
-    try {
-        const userId = res.locals.user._id;
-        await OrderService.addItemCart(req, userId);
-        return res.status(201).json({
-            status: true,
-            data: null,
-            message: "Request successful",
-        });
-    } catch (error) {
-        next(error);
-    }
+     try {
+          const userId = res.locals.user._id;
+          await OrderService.addItemCart(req, userId);
+          return res.status(201).json({
+               status: true,
+               data: null,
+               message: "Request successful",
+          });
+     } catch (error) {
+          next(error);
+     }
 };
 const updateCartItem = async (req, res, next) => {
-    try {
-        const userId = res.locals.user._id;
-        await OrderService.updateCartItem(req, userId);
-        return res.status(200).json({
-            status: true,
-            data: null,
-            message: "Request successful",
-        });
-    } catch (error) {
-        next(error);
-    }
+     try {
+          const userId = res.locals.user._id;
+          await OrderService.updateCartItem(req, userId);
+          return res.status(200).json({
+               status: true,
+               data: null,
+               message: "Request successful",
+          });
+     } catch (error) {
+          next(error);
+     }
 };
 const getUserCart = async (req, res, next) => {
-    try {
-        const userId = res.locals.user._id;
-        const { data } = await OrderService.getUserCartItems(userId);
-        return res.status(200).json({
-            status: true,
-            data,
-            message: "Request successful",
-        });
-    } catch (error) {
-        next(error);
-    }
+     try {
+          const userId = res.locals.user._id;
+          const { data } = await OrderService.getUserCartItems(userId);
+          return res.status(200).json({
+               status: true,
+               data,
+               message: "Request successful",
+          });
+     } catch (error) {
+          next(error);
+     }
 };
 const createOrder = async (req, res, next) => {
-    try {
-        const userId = res.locals.user._id;
-        await OrderService.createOrder(req, userId);
-        return res.status(201).json({
-            status: true,
-            data: null,
-            message: "Request successful",
-        });
-    } catch (error) {
-        next(error);
-    }
+     try {
+          const userId = res.locals.user._id;
+          await OrderService.createOrder(req, userId);
+          return res.status(201).json({
+               status: true,
+               data: null,
+               message: "Request successful",
+          });
+     } catch (error) {
+          next(error);
+     }
 };
 const getOrder = async (req, res, next) => {
-    try {
-        const { data } = await OrderService.getOrder(req);
-        return res.status(200).json({
-            status: true,
-            data,
-            message: "Request successful",
-        });
-    } catch (error) {
-        next(error);
-    }
+     try {
+          const { data } = await OrderService.getOrder(req);
+          return res.status(200).json({
+               status: true,
+               data,
+               message: "Request successful",
+          });
+     } catch (error) {
+          next(error);
+     }
 };
 const getOrders = async (req, res, next) => {
-    try {
-        const userId = res.locals.user._id;
-        const { data } = await OrderService.getOrdersPlaced(req, userId);
-        return res.status(200).json({
-            status: true,
-            data,
-            message: "Request successful",
-        });
-    } catch (error) {
-        next(error);
-    }
+     try {
+          const userId = res.locals.user._id;
+          const { data } = await OrderService.getOrdersPlaced(req, userId);
+          return res.status(200).json({
+               status: true,
+               data,
+               message: "Request successful",
+          });
+     } catch (error) {
+          next(error);
+     }
 };
 
 const updateOrder = async (req, res, next) => {
-    try {
-        await OrderService.editOrder(req);
-        return res.status(200).json({
-            status: true,
-            data: null,
-            message: "Request successful",
-        });
-    } catch (error) {
-        next(error);
-    }
+     try {
+          await OrderService.editOrder(req);
+          return res.status(200).json({
+               status: true,
+               data: null,
+               message: "Request successful",
+          });
+     } catch (error) {
+          next(error);
+     }
 };
 
 const deleteOrder = async (req, res, next) => {
-    try {
-        await OrderService.deleteOrder(req);
-        return res.status(200).json({
-            status: true,
-            data: null,
-            message: "Request successful",
-        });
-    } catch (error) {
-        next(error);
-    }
+     try {
+          await OrderService.deleteOrder(req);
+          return res.status(200).json({
+               status: true,
+               data: null,
+               message: "Request successful",
+          });
+     } catch (error) {
+          next(error);
+     }
 };
 const clearOrderCart = async (req, res, next) => {
-    try {
-        await OrderService.clearOrderCart(res.locals.user._id);
-        return res.status(200).json({
-            status: true,
-            data: null,
-            message: "Request successful",
-        });
-    } catch (error) {
-        next(error);
-    }
+     try {
+          await OrderService.clearCartItems(res.locals.user._id);
+          return res.status(200).json({
+               status: true,
+               data: null,
+               message: "Request successful",
+          });
+     } catch (error) {
+          next(error);
+     }
 };
 
 export default {
-    createOrder,
-    getOrder,
-    addToCart,
-    getUserCart,
-    getOrders,
-    updateOrder,
-    deleteOrder,
-    clearOrderCart,
+     createOrder,
+     getOrder,
+     addToCart,
+     getUserCart,
+     getOrders,
+     updateOrder,
+     deleteOrder,
+     clearOrderCart,
 };
--- src/controllers/app/user.controllers.js
+++ src/controllers/app/user.controllers.js
@@ -1,62 +1,62 @@
 import UserService from "../../services/app/user.service.js";
 
 const userProfileDetails = async (req, res, next) => {
-    try {
-        const userId = res.locals.user._id;
-        const { data } = await UserService.getUserProfile(userId);
-        return res.status(200).json({
-            status: true,
-            data,
-            message: "Request successful",
-        });
-    } catch (error) {
-        next(error);
-    }
+     try {
+          const userId = res.locals.user._id.toString();
+          const { data } = await UserService.getUserProfile(userId);
+          return res.status(200).json({
+               status: true,
+               data,
+               message: "Request successful",
+          });
+     } catch (error) {
+          next(error);
+     }
 };
 
 const updateUser = async (req, res, next) => {
-    try {
-        const userId = res.locals.user._id;
-        await UserService.updateUserDetails(userId, { ...req.body });
-        return res.status(200).json({
-            status: true,
-            data: null,
-            message: "Request successful",
-        });
-    } catch (error) {
-        next(error);
-    }
+     try {
+          const userId = res.locals.user._id;
+          await UserService.updateUserDetails(userId, { ...req.body });
+          return res.status(200).json({
+               status: true,
+               data: null,
+               message: "Request successful",
+          });
+     } catch (error) {
+          next(error);
+     }
 };
 
 const verifyCustomerData = async (req, res, next) => {
-    try {
-        await UserService.verifyCustomerData({ ...req.body });
-        return res.status(200).json({
-            status: true,
-            data: null,
-            message: "Request successful",
-        });
-    } catch (error) {
-        next(error);
-    }
+     try {
+          await UserService.verifyCustomerData({ ...req.body });
+          return res.status(200).json({
+               status: true,
+               data: null,
+               message: "Request successful",
+          });
+     } catch (error) {
+          next(error);
+     }
 };
 const logoutUser = async (req, res, next) => {
-    try {
-        const user = res.locals.user;
-        const { data } = await UserService.logoutUser(user);
-        return res.status(200).json({
-            status: true,
-            data,
-            message: "Request successful",
-        });
-    } catch (error) {
-        next(error);
-    }
+     try {
+          const user = res.locals.user;
+          const { data } = await UserService.logoutUser(user);
+          return res.status(200).json({
+               status: true,
+               data,
+               message: "Request successful",
+          });
+     } catch (error) {
+          next(error);
+     }
 };
 
 export default {
-    updateUser,
-    verifyCustomerData,
-    userProfileDetails,
-    logoutUser,
+     updateUser,
+     verifyCustomerData,
+     userProfileDetails,
+     logoutUser,
 };
