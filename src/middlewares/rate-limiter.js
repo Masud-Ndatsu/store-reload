cf5af5472f6e9fe6155426950fb860c6592cc585
Masud-Ndatsu <masudndatsu@gmail.com>
2023-10-17 10:24:26 +0100

    feat: modified rate limiter middleware

--- src/middlewares/rate-limiter.js
+++ src/middlewares/rate-limiter.js
@@ -1,9 +1,9 @@
 import rateLimiter from "express-rate-limit";
 
 const limiter = rateLimiter({
-    windowMs: 1 * 60 * 1000, // 1 minutes
-    max: 10,
-    message: "Too many requests from this IP, please try again later.",
+     windowMs: 1 * 60 * 1000, // 1 minutes
+     max: 15,
+     message: "Too many requests from this IP, please try again later.",
 });
 
 export default limiter;
