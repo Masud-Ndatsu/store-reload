4bd1d5e3b2eb7c9ba1f20522c1b4dad7c58f1f60
Masud-Ndatsu <masudndatsu@gmail.com>
2023-10-17 10:15:12 +0100

    added get support messages

--- src/controllers/admin/category.controllers.js
+++ src/controllers/admin/category.controllers.js
@@ -1,27 +1,27 @@
 import { CategoryService } from "../../services/admin/category.service";
 
 export const createCategory = async (req, res, next) => {
-  try {
-    await CategoryService.createCategory(req.body);
-    return res.status(201).json({
-      status: true,
-      data: null,
-      message: "Request successful",
-    });
-  } catch (error) {
-    next(error);
-  }
+     try {
+          await CategoryService.createCategory(req.body);
+          return res.status(200).json({
+               status: true,
+               data: null,
+               message: "Request successful",
+          });
+     } catch (error) {
+          next(error);
+     }
 };
 
 export const getCategories = async (req, res, next) => {
-  try {
-    const { data } = await CategoryService.getCategories();
-    return res.status(200).json({
-      status: true,
-      data,
-      message: "Request successful",
-    });
-  } catch (error) {
-    next(error);
-  }
+     try {
+          const { data } = await CategoryService.getCategories();
+          return res.status(200).json({
+               status: true,
+               data,
+               message: "Request successful",
+          });
+     } catch (error) {
+          next(error);
+     }
 };
--- src/controllers/admin/user.controllers.js
+++ src/controllers/admin/user.controllers.js
@@ -1,61 +1,75 @@
 import UserService from "../../services/admin/user.service";
 
 const getAllUsers = async (req, res, next) => {
-    try {
-        const { data } = await UserService.getAllUsers(req);
-        return res.status(200).json({
-            status: true,
-            data,
-            message: "Request successful",
-        });
-    } catch (error) {
-        next(error);
-    }
+     try {
+          const { data } = await UserService.getAllUsers(req);
+          return res.status(200).json({
+               status: true,
+               data,
+               message: "Request successful",
+          });
+     } catch (error) {
+          next(error);
+     }
 };
 const getUser = async (req, res, next) => {
-    try {
-        const { data } = await UserService.getUser(req);
-        return res.status(200).json({
-            status: true,
-            data,
-            message: "Request successful",
-        });
-    } catch (error) {
-        next(error);
-    }
+     try {
+          const { data } = await UserService.getCustomer(req);
+          return res.status(200).json({
+               status: true,
+               data,
+               message: "Request successful",
+          });
+     } catch (error) {
+          next(error);
+     }
 };
 
 const getUserProfile = async (req, res, next) => {
-    try {
-        const userId = res.locals.user._id;
+     try {
+          const user_id = res.locals.user._id;
 
-        const { data } = await UserService.getUserProfile(userId);
-        return res.status(200).json({
-            status: true,
-            data,
-            message: "Request successful",
-        });
-    } catch (error) {
-        next(error);
-    }
+          const { data } = await UserService.getUserProfile(user_id);
+          return res.status(200).json({
+               status: true,
+               data,
+               message: "Request successful",
+          });
+     } catch (error) {
+          next(error);
+     }
 };
 
 const updateUser = async (req, res, next) => {
-    try {
-        await UserService.updateUser(req, res.locals.user._id);
-        return res.status(200).json({
-            status: true,
-            data: null,
-            message: "Request successful",
-        });
-    } catch (error) {
-        next(error);
-    }
+     try {
+          await UserService.updateUser(req, res.locals.user._id);
+          return res.status(200).json({
+               status: true,
+               data: null,
+               message: "Request successful",
+          });
+     } catch (error) {
+          next(error);
+     }
+};
+
+const getSupportMessages = async (_req, res, next) => {
+     try {
+          const { data } = await UserService.getSupportMessages();
+          return res.status(200).json({
+               status: true,
+               data,
+               message: "Request successful",
+          });
+     } catch (error) {
+          next(error);
+     }
 };
 
 export default {
-    getAllUsers,
-    getUser,
-    getUserProfile,
-    updateUser,
+     getAllUsers,
+     getUser,
+     getUserProfile,
+     updateUser,
+     getSupportMessages,
 };
