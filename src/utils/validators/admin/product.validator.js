503690ef28cab2123f9a6d1da24bebbdecd9cef0
Masud-Ndatsu <masudndatsu@gmail.com>
2023-10-17 10:37:55 +0100

    feat: added inventry and manufacturer to validator

--- src/utils/validators/admin/product.validator.js
+++ src/utils/validators/admin/product.validator.js
@@ -1,21 +1,23 @@
 import Joi from "joi";
 
 export const createProductSchema = Joi.object({
-  name: Joi.string().required().trim(),
-  description: Joi.string().required().trim(),
-  category: Joi.string().required().trim(),
-  image: Joi.object().optional(),
-  type: Joi.string().trim(),
-  price: Joi.string().required(),
-  tags: Joi.string().required().trim(),
+     name: Joi.string().required().trim(),
+     description: Joi.string().required().trim(),
+     category: Joi.string().required().trim(),
+     image: Joi.object().optional(),
+     type: Joi.string().trim(),
+     price: Joi.string().required(),
+     tags: Joi.string().required().trim(),
+     manufacturer: Joi.string().required().trim(),
+     inventry: Joi.string().required(),
 });
 
 export const editProductSchema = Joi.object({
-  name: Joi.string().trim(),
-  description: Joi.string().trim(),
-  category: Joi.string().trim(),
-  image: Joi.object().optional(),
-  type: Joi.string().trim(),
-  price: Joi.string().optional(),
-  tags: Joi.string().trim(),
+     name: Joi.string().trim(),
+     description: Joi.string().trim(),
+     category: Joi.string().trim(),
+     image: Joi.object().optional(),
+     type: Joi.string().trim(),
+     price: Joi.string().optional(),
+     tags: Joi.string().trim(),
 });
