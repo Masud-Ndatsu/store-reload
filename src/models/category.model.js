c01ee5bfab450229414c85b2a355536e4e1f34d8
Masud-Ndatsu <masudndatsu@gmail.com>
2023-10-17 10:05:26 +0100

    feat: modified model fields to snake case

--- src/models/category.model.js
+++ src/models/category.model.js
@@ -3,17 +3,17 @@ import mongoose from "mongoose";
 const { Schema, model } = mongoose;
 
 const schema = new Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-        },
-        productType: {
-            type: String,
-            required: true,
-        },
-    },
-    { timestamps: true }
+     {
+          name: {
+               type: String,
+               required: true,
+          },
+          product_type: {
+               type: String,
+               required: true,
+          },
+     },
+     { timestamps: true }
 );
 
 schema.index({ name: 1 }, { unique: true });
