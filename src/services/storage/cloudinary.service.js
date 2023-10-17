d36a6a129b5fd4d37efe7b678ff46e42266bd382
Masud-Ndatsu <masudndatsu@gmail.com>
2023-10-17 20:01:54 +0100

    modified storage service to delete image files loaded in memory on any error

--- src/services/storage/cloudinary.service.js
+++ src/services/storage/cloudinary.service.js
@@ -3,49 +3,53 @@ import config from "../../config/index.js";
 import fs from "fs";
 
 cloudinary.config({
-  cloud_name: config.cloud.name,
-  api_key: config.cloud.api_key,
-  api_secret: config.cloud.api_secret,
+     cloud_name: config.cloud.name,
+     api_key: config.cloud.api_key,
+     api_secret: config.cloud.api_secret,
 });
 
 const _upload = (file, folder) => {
-  return new Promise((resolve, reject) => {
-    cloudinary.v2.uploader.upload(
-      file,
-      {
-        resource_type: "auto",
-        folder,
-        transformation: { width: 640, height: 640, crop: "fill" },
-      },
-      (error, result) => {
-        if (error) {
-          reject({ error });
-        }
-        resolve({ url: result.url, id: result.public_id });
-      }
-    );
-  });
+     return new Promise((resolve, reject) => {
+          cloudinary.v2.uploader.upload(
+               file,
+               {
+                    resource_type: "auto",
+                    folder,
+                    transformation: { width: 640, height: 640, crop: "fill" },
+               },
+               (error, result) => {
+                    if (error) {
+                         reject({ error });
+                    }
+                    resolve({ url: result.url, id: result.public_id });
+               }
+          );
+     });
 };
 
 export const uploadFile = async (req) => {
-  try {
-    const urls = [];
-    const files = req.files;
+     try {
+          const urls = [];
+          const files = req.files;
 
-    for (const file of files) {
-      const { path } = file;
-      const uploader = async (path) => await _upload(path, "storereload");
-      const newpath = await uploader(path);
-      urls.push(newpath);
-      fs.unlinkSync(path);
-    }
-    const url = urls.map((file) => {
-      return file.url;
-    });
+          for (const file of files) {
+               const { path } = file;
+               const uploader = async (path) =>
+                    await _upload(path, "storereload");
+               const newpath = await uploader(path);
+               urls.push(newpath);
+               fs.unlinkSync(path);
+          }
+          const url = urls.map((file) => {
+               return file.url;
+          });
 
-    return url;
-  } catch (error) {
-    console.log("IMAGE ERROR: ", error);
-    throw error;
-  }
+          return url;
+     } catch (error) {
+          for (let file of req.files) {
+               fs.unlinkSync(file.path);
+          }
+          console.log("IMAGE ERROR: ", error);
+          throw error;
+     }
 };
