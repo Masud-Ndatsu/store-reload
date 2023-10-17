8e990026b1510229a8ee5f381cdc58787a2dea57
Masud-Ndatsu <masudndatsu@gmail.com>
2023-10-17 19:12:59 +0100

    optimized db query and

--- src/services/admin/category.service.js
+++ src/services/admin/category.service.js
@@ -2,19 +2,32 @@ import { AppError } from "../../errors";
 import { Category } from "../../models/category.model";
 
 export class CategoryService {
-    static createCategory = async (categoryReq) => {
-        const { name } = categoryReq;
+     static createCategory = async (body) => {
+          const { name } = body;
 
-        const category = await Category.findOne({ name }).select("_id").lean();
+          const category = await Category.findOne({ name })
+               .select("_id")
+               .lean();
 
-        if (category) {
-            throw new AppError("Category already exists", 400);
-        }
-        await Category.create({ ...categoryReq });
-        return;
-    };
-    static getCategories = async () => {
-        const category = await Category.aggregate([{ $match: {} }]);
-        return { data: category };
-    };
+          if (category) {
+               throw new AppError("Category already exists", 400);
+          }
+          await new Category({
+               ...body,
+          }).save();
+
+          return;
+     };
+
+     static getCategories = async () => {
+          const category = await Category.aggregate([
+               {
+                    $match: {},
+               },
+          ]);
+
+          return {
+               data: category,
+          };
+     };
 }
