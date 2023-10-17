142a91e48a468165d7c564b07e68b2304d8e18b9
Masud-Ndatsu <masudndatsu@gmail.com>
2023-10-17 10:22:36 +0100

    feat: modified customer auth middleware

--- src/middlewares/_app/auth.middlewares.js
+++ src/middlewares/_app/auth.middlewares.js
@@ -3,26 +3,29 @@ import { ShopModel } from "../../models/shop.model.js";
 import { decodeToken } from "../../utils/auth.utils.js";
 
 export const authUser = async (req, res, next) => {
-    try {
-        let token = req.header("Authorization");
+     try {
+          let token = req.header("Authorization");
 
-        if (!token) {
-            throw new AppError("token is required", 400);
-        }
-        const [, authToken] = req.header("Authorization").split(" ");
+          if (!token) {
+               throw new AppError("token is required", 400);
+          }
+          const [, authToken] = req.header("Authorization").split(" ");
 
-        const decoded = await decodeToken(authToken, process.env.APP_SIGNATURE);
+          const decoded = await decodeToken(
+               authToken,
+               process.env.APP_SIGNATURE
+          );
 
-        const user = await ShopModel.findById(decoded.id).lean();
+          const user = await ShopModel.findById(decoded.id).lean();
 
-        if (!user) {
-            throw new AppError("Unauthorized user", 401);
-        }
+          if (!user) {
+               throw new AppError("Unauthorized user", 401);
+          }
 
-        res.locals.user = user;
-        return next();
-    } catch (error) {
-        console.log("ERROR", error);
-        next(error);
-    }
+          res.locals.user = user;
+          return next();
+     } catch (error) {
+          console.log("ERROR", error);
+          next(error);
+     }
 };
