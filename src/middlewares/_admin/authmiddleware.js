9dbe1d22b2aeb9198f2a366c8fd99f22d48c2772
Masud-Ndatsu <masudndatsu@gmail.com>
2023-10-17 10:21:54 +0100

    feat: modified admin auth middleware

--- src/middlewares/_admin/authmiddleware.js
+++ src/middlewares/_admin/authmiddleware.js
@@ -1,28 +1,31 @@
 import { AppError } from "../../errors/index.js";
-import { Admin } from "../../models/admin.model.js";
+import { User } from "../../models/user.model.js";
 import { decodeToken } from "../../utils/auth.utils.js";
 
 export const authAdminUser = async (req, res, next) => {
-    try {
-        let token = req.header("Authorization");
+     try {
+          let token = req.header("Authorization");
 
-        if (!token) {
-            throw new AppError("token is required", 400);
-        }
-        const [, authToken] = req.header("Authorization").split(" ");
+          if (!token) {
+               throw new AppError("token is required", 400);
+          }
+          const [, authToken] = req.header("Authorization").split(" ");
 
-        const decoded = await decodeToken(authToken, process.env.ADMIN_SIGNATURE);
+          const decoded = await decodeToken(
+               authToken,
+               process.env.ADMIN_SIGNATURE
+          );
 
-        const user = await Admin.findById(decoded.id).lean();
+          const user = await User.findById(decoded.id).lean();
 
-        if (!user) {
-            throw new AppError("Unauthorized user", 401);
-        }
-        res.locals.user = user;
-        return next();
-    } catch (error) {
-        console.log("ERROR", error);
+          if (!user) {
+               throw new AppError("Unauthorized user", 401);
+          }
+          res.locals.user = user;
+          return next();
+     } catch (error) {
+          console.log("ERROR", error);
 
-        next(error);
-    }
+          next(error);
+     }
 };
